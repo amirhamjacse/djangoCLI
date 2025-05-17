f26685f79d1f6c9f2f95f9cccba079441e9fec1b
amirhamjacse <amirhamza824@gmail.com>
2025-05-17 15:44:52 +0600

    updated

--- CONFIG/urls.py
+++ CONFIG/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myappcli import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('qu', views.test_view, name='queue')
 ]
